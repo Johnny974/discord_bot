c915120b12fe5d53d69378fa16b3429fa5590d4c
Johnny974 <jan.okal10@gmail.com>
2025-09-08 22:49:00 +0200

    debug continues

--- nasa_api.py
+++ nasa_api.py
@@ -2,12 +2,13 @@ import requests
 import os
 
 
-NASA_KEY = os.environ.get('NASA_KEY')
+NASA_KEY = os.getenv('NASA_KEY')
 NASA_APOD_URL = f"https://api.nasa.gov/planetary/apod?api_key={NASA_KEY}"
 
 
 def get_apod_data():
     response = requests.get(NASA_APOD_URL)
+    print(response.json())
     data = response.json()
 
     return data
