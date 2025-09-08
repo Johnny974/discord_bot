61adc99811ded49e9865abecda5f2b7b125c377a
Johnny974 <jan.okal10@gmail.com>
2025-09-08 22:58:38 +0200

    debug

--- nasa_api.py
+++ nasa_api.py
@@ -5,6 +5,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 NASA_KEY = os.environ.get('NASA_KEY')
+print("NASA_API_KEY =", NASA_KEY)
 NASA_APOD_URL = f"https://api.nasa.gov/planetary/apod?api_key={NASA_KEY}"
 
 
