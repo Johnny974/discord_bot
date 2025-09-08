c39ee970c260adbdec581e228f1056aacfa40fba
Johnny974 <jan.okal10@gmail.com>
2025-09-08 23:01:44 +0200

    last try of debug

--- nasa_api.py
+++ nasa_api.py
@@ -5,13 +5,14 @@ from dotenv import load_dotenv
 load_dotenv()
 
 NASA_KEY = os.environ.get('NASA_KEY')
-print("NASA_API_KEY =", NASA_KEY)
+# print("NASA_API_KEY =", NASA_KEY)
 NASA_APOD_URL = f"https://api.nasa.gov/planetary/apod?api_key={NASA_KEY}"
 
 
 def get_apod_data():
     response = requests.get(NASA_APOD_URL)
-    print(response.json())
+    # print(response.json())
     data = response.json()
 
     return data
+
